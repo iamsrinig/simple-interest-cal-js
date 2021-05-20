620015e74a272d3394b5c5be0ea25214dfb52390
Srinivas Gurajala <srinivas.gurajala@gmail.com>
2021-05-20 06:18:31 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function calculate() {
-    p = document.getElementById("p").value;
-    n = document.getElementById("n").value;
-    r = document.getElementById("r").value;
+function calculate_simple_interest() {
+    p = document.getElementById("principal_bal").value;
+    n = document.getElementById("no_of_years").value;
+    r = document.getElementById("rate_of_interest").value;
     result = document.getElementById("result");
     result.innerHTML = "The interest is " + (p*n*r/100);
   }
