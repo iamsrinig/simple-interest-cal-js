4dcc01faf933f421ad71c044bf4aa6417521008d
Srinivas Gurajala <srinivas.gurajala@gmail.com>
2021-05-20 07:09:24 +0000

    updated styles

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
 function calculate_simple_interest() {
-    p = document.getElementById("principal_bal").value;
-    n = document.getElementById("no_of_years").value;
-    r = document.getElementById("rate_of_interest").value;
-    result = document.getElementById("result");
-    result.innerHTML = "The interest is " + (p*n*r/100);
-  }
+  p = document.getElementById("principal_bal").value;
+  n = document.getElementById("no_of_years").value;
+  r = document.getElementById("myRange").value;
+  result = document.getElementById("result");
+  result.innerHTML = "The interest is " + (p*n*r/100);
+}
